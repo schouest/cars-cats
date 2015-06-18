4e993b24efbdfa4fe1dbe8361c5949bc636397fb
Stephen <e2stephen@cox.net>
2015-06-18 15:17:28 -0700

    html framework/  basic navigation

--- app.js
+++ app.js
@@ -15,21 +15,21 @@ var server = http.createServer(function (request, response){
         });
     }
     else if(request.url === '/cats') {
-        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+        fs.readFile('cats.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
         });
     }
     else if(request.url === '/cars') {
-        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+        fs.readFile('cars.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
         });
     }
     else if(request.url === '/cars/new') {
-        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+        fs.readFile('newcar.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
             response.write(contents);  //  send response body
             response.end(); // finished!
